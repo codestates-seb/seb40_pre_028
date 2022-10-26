9603b48c7bc2d2345136cee8060f070ab1699a10
Shin-seung-hyun <jjack1028@naver.com>
2022-10-26 17:12:36 +0900

    #34 feat: User Entity & Mapping

---
+++ server/src/main/java/com/seb40/server/User/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.seb40.server.User.repository;
+
+import com.seb40.server.User.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+    Optional<UserEntity> findByUser_email(String user_email);
+}
