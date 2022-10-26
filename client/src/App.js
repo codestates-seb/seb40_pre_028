91903cd5e421193e9542b6f219903f4603283b07
kihyeoon <bangion93@gmail.com>
2022-10-26 22:35:33 +0900

    #18 style: 버튼 컴포넌트 완성

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,17 @@
-import GlobalStyle from "./assets/style/GlobalStyle";
-import Header from "./components/Header";
-import LogIn from "./pages/Login";
-import SignUp from "./pages/Signup";
-import Footer from "./components/Footer";
-import { Nav } from "./components/Nav";
+import GlobalStyle from './assets/style/GlobalStyle';
+import Header from './components/Header';
+// import LogIn from './pages/Login';
+// import SignUp from './pages/Signup';
+import Footer from './components/Footer';
+import { Nav } from './components/Nav';
 
 function App() {
   return (
     <div>
       <GlobalStyle />
       <Header />
-      <SignUp />
-      <LogIn />
+      {/* <SignUp /> */}
+      {/* <LogIn /> */}
       <Nav />
       <Footer />
     </div>
