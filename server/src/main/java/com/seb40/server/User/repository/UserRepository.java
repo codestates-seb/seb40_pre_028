6ba12ec732ff2e92f56c5fa01631fd333d5d0f11
Shin-seung-hyun <jjack1028@naver.com>
2022-10-27 10:04:47 +0900

    refactor: 코드 디렉토리 리팩토링

--- server/src/main/java/com/seb40/server/User/repository/UserRepository.java
+++ server/src/main/java/com/seb40/server/User/repository/UserRepository.java
@@ -1,12 +1,12 @@
 package com.seb40.server.User.repository;
 
-import com.seb40.server.User.entity.UserEntity;
+import com.seb40.server.User.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
-    Optional<UserEntity> findByEmail(String email);
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
