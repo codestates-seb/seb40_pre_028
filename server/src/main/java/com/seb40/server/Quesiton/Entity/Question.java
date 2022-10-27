72ae1b7ca367926486cab470d288e928d2c1aa3c
kyh0113 <cin05189@gmail.com>
2022-10-27 15:05:27 +0900

    #31 refactor:refactor1

--- server/src/main/java/com/seb40/server/Quesiton/Entity/Question.java
+++ server/src/main/java/com/seb40/server/Quesiton/Entity/Question.java
@@ -5,13 +5,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Getter
