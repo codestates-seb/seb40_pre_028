8e43d2122786cf8fa50e15754192753c9b054289
kyh0113 <cin05189@gmail.com>
2022-10-28 10:34:20 +0900

    1028morning

--- server/src/main/java/com/seb40/server/Quesiton/Entity/Question.java
+++ server/src/main/java/com/seb40/server/Quesiton/Entity/Question.java
@@ -4,11 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -19,7 +22,11 @@ public class Question {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long questionId;
+    private long userId;
     private String questionTitle;
     private String questionBody;
-
+    @CreatedDate
+    private LocalDateTime questionCreatedAt;
+    @LastModifiedDate
+    private LocalDateTime questionModified;
 }
