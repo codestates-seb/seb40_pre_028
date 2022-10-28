82efce0c350e44e793317011d50301c263f64205
kyh0113 <cin05189@gmail.com>
2022-10-28 16:56:10 +0900

    refactor: vote는 포함되지 않았습니다

--- server/src/main/java/com/seb40/server/Quesiton/Entity/Question.java
+++ server/src/main/java/com/seb40/server/Quesiton/Entity/Question.java
@@ -1,22 +1,27 @@
 package com.seb40.server.Quesiton.Entity;
 
+import com.seb40.server.Answer.Entity.Answer;
+import com.seb40.server.User.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-public class Question {  // (1)
-    @Id// (2)
+public class Question extends BaseTimeEntity{
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long questionId;
     private String questionTitle;
