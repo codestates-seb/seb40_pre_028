c83b4cff5e3594fe97fae44dd07e4e374adfc355
x-xuhxun <hiihii0402@gmail.com>
2022-10-28 13:52:43 +0900

    editor 추가

--- client/src/components/QuestionDetails/DetailAnswer.js
+++ client/src/components/QuestionDetails/DetailAnswer.js
@@ -1,13 +1,33 @@
 import styled from 'styled-components';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { DetailMain } from './DetailMain';
+
+const Container = styled.div`
+  h2 {
+    font-size: 20px;
+  }
+  .qanswer {
+    padding-left: 20px;
+  }
+`;
 
 const Answer = styled.div`
   * {
     margin: 20px 0;
   }
-  h2 {
-    font-size: 20px;
+  .ck.ck-editor {
+    max-width: 100%;
+  }
+  // 에디터 쓸 있는공간(네모박스)
+  .ck-editor__editable {
+    min-height: 300px;
+  }
+  .ck-content .marker-yellow {
+    background-color: var(--ck-highlight-marker-yellow);
+  }
+  a {
+    color: #0074cc;
   }
 `;
 
@@ -24,31 +44,41 @@ const Btn = styled.button`
   &:hover {
     background: #0074cc;
   }
+  margin-top: 10px;
 `;
 
 export const DetailAnswer = () => {
   return (
-    <Answer>
-      <p>Know someone who can answer? Share a link to this question via email, Twitter, or Facebook.</p>
-      <h2>Your Answer</h2>
-      <CKEditor
-        editor={ClassicEditor}
-        data="<p>Hello from CKEditor 5!</p>"
-        onReady={editor => {
-          console.log('Editor is ready to use!', editor);
-        }}
-        onChange={(event, editor) => {
-          const data = editor.getData();
-          console.log({ event, editor, data });
-        }}
-        onBlur={(event, editor) => {
-          console.log('Blur.', editor);
-        }}
-        onFocus={(event, editor) => {
-          console.log('Focus.', editor);
-        }}
-      />
-      <Btn>Post Your Answer</Btn>
-    </Answer>
+    <Container>
+      <h2 className="qanswer">1 Answer</h2>
+      <DetailMain />
+      <Answer>
+        <p>
+          Know someone who can answer? Share a link to this <a href="/question">question</a> via <a href="email">email</a>,{' '}
+          <a href="/twitter">Twitter</a>, or <a href="/Facebook">Facebook</a>.
+        </p>
+        <h2>Your Answer</h2>
+        <CKEditor
+          editor={ClassicEditor}
+          config={{
+            placeholder: '내용을 입력하세요.',
+          }}
+          onReady={editor => {
+            console.log('Editor is ready to use!', editor);
+          }}
+          onChange={(event, editor) => {
+            const data = editor.getData();
+            console.log({ event, editor, data });
+          }}
+          onBlur={(event, editor) => {
+            console.log('Blur.', editor);
+          }}
+          onFocus={(event, editor) => {
+            console.log('Focus.', editor);
+          }}
+        />
+        <Btn>Post Your Answer</Btn>
+      </Answer>
+    </Container>
   );
 };
