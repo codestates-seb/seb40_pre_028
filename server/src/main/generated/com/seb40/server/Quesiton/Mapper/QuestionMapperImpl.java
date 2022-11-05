79ec5348c5f511b55ff7fe8291f127abb3ae889a
kyh0113 <cin05189@gmail.com>
2022-11-05 18:18:17 +0900

    questionVoteSum추가

--- server/src/main/generated/com/seb40/server/Quesiton/Mapper/QuestionMapperImpl.java
+++ server/src/main/generated/com/seb40/server/Quesiton/Mapper/QuestionMapperImpl.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-11-04T11:58:47+0900",
+    date = "2022-11-05T18:16:04+0900",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.17 (Azul Systems, Inc.)"
 )
 @Component
--- server/src/main/java/com/seb40/server/Quesiton/Dto/QuestionResponseDto.java
+++ server/src/main/java/com/seb40/server/Quesiton/Dto/QuestionResponseDto.java
@@ -24,7 +24,7 @@ public class QuestionResponseDto { // userId라고 하면 DB에서 자동으로 
     private String name;
     private int answerNum;
     private long views;
-
+    private long questionVoteSum;
     private List<QuestionTag> questionTags;
 
 }
