9290d7b905b8b233e1464cef95059fdd6b5a7b91
Shin-seung-hyun <jjack1028@naver.com>
2022-11-07 10:46:17 +0900

    refactor: Vote controller refactor

--- server/src/main/java/com/seb40/server/Vote/QuestionVote/Controller/QuestionVoteController.java
+++ server/src/main/java/com/seb40/server/Vote/QuestionVote/Controller/QuestionVoteController.java
@@ -8,7 +8,6 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import javax.validation.Valid;
 import java.util.Iterator;
 import java.util.List;
